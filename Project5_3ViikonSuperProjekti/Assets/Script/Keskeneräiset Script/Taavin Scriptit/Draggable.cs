fa2e6ae9c19acbb71bda892599035490b60f09d7
Mutti88 <juomakuski@hotmail.com>
2017-11-14 14:43:41 +0200

    kortti systemin alku

---
+++ Project5_3ViikonSuperProjekti/Assets/Script/Keskeneräiset Script/Taavin Scriptit/Draggable.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+   public Transform parentToReturnTo = null;
+   public Transform placeholderParent = null;
+   GameObject placeholder = null;
+  /* public enum Slot { WEAPON, HEAD, CHEST, LEGS, FEET, INVENTORY };
+   public Slot typeOfItem = Slot.WEAPON;*/
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+        parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
+        this.transform.SetParent(this.transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        
+
+        //Voi tehdä esim hohto efektin dropzone:lle
+        //DropZone[] zones =  GameObject.FindObjectsOfType<DropZone>();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        this.transform.position = eventData.position;
+        if (placeholder.transform.parent != placeholderParent)
+            placeholderParent.transform.SetParent(placeholderParent);
+        int newSiblingindex = placeholderParent.childCount;
+
+        for (int i = 0; i < placeholderParent.childCount; i++)
+        {
+            if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                //placeholder.transform.SetSiblingIndex(i);
+                newSiblingindex = i;
+
+                if (placeholder.transform.GetSiblingIndex() < newSiblingindex)
+                {
+                    newSiblingindex--;
+                }
+                break;
+            }
+        }
+        placeholder.transform.SetSiblingIndex(newSiblingindex);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        Destroy(placeholder);
+        //Ota hohto efekti pois päältä jos tehty
+        //Tsekkailee mitä on kortin alla.
+        //EventSystem.current.RaycastAll(eventData)
+
+    }
+}
---
+++ Project5_3ViikonSuperProjekti/Assets/Script/Keskeneräiset Script/Taavin Scriptit/DropZone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {    //ENABLE MYÖHEMMIN
+    //public Draggable.Slot typeOfItem = Draggable.Slot.INVENTORY;
+    //linkki tutoriaalin kolmanteen osaan https://youtu.be/AM7wBz9azyU
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+          Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+          if (d != null)
+          {
+              d.placeholderParent = this.transform;
+          }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
+        }
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "was dropped on"+ gameObject.name);
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+
+            //Tutustu tähän myöhemmin
+            /*if (typeOfItem == d.typeOfItem || typeOfItem == Draggable.Slot.INVENTORY)
+            {
+                // d.parentToReturnTo = this.transform;
+            }*/
+        }
+    }
+}
