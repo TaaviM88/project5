c30334a08b69851280b6029bb50bf078314d62ca
DESKTOP-AA7SO01\lauri <lauri.syvanen@hotmail.com>
2017-11-28 09:11:59 +0200

    Skyboxit, particle fog

---
+++ Project5_3ViikonSuperProjekti/Assets/Script/tmp_scriptit/Laurin tekeillä olevat/CameraManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	public Transform player1, player2;
+	public float minSizeY = 5f;
+
+	void SetCameraPos() {
+		Vector3 middle = (player1.position + player2.position) * 0.5f;
+
+		GetComponent<Camera>().transform.position = new Vector3(
+			middle.x,
+			middle.y,
+			GetComponent<Camera>().transform.position.z
+		);
+	}
+
+	void SetCameraSize() {
+		//Koko on näytön reso
+		float minSizeX = minSizeY * Screen.width / Screen.height;
+
+
+		float width = Mathf.Abs(player1.position.x - player2.position.x) * 0.85f;
+		float height = Mathf.Abs(player1.position.y - player2.position.y) * 0.85f;
+
+		//koon laskenta
+		float camSizeX = Mathf.Max(width, minSizeX);
+		GetComponent<Camera>().fieldOfView = Mathf.Max(height,
+			camSizeX * Screen.height / Screen.width, minSizeY);
+	}
+
+	void Update() {
+		SetCameraPos();
+		SetCameraSize();
+	}
+
+}
---
+++ Project5_3ViikonSuperProjekti/Assets/Script/tmp_scriptit/Laurin tekeillä olevat/Player2Movement.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour {
+
+	private float inputDirection; // x value of our movevector
+	private float verticalVelocity; // Y value of our move vector
+
+	public float jumpForce = 10.0f;
+	public float speed = 5.0f;
+	public float gravity = 1.0f;
+	public float hoverHeight = 0.2f;
+	private bool secondJumpAvail = false;
+	private bool _facingRight = true;
+	public float value;
+
+	//AnimeController _animeScript;
+	private Animator anime;
+	private Rigidbody rigidbody;
+	private Vector3 moveVector;
+	//lastmotionilla lukittiin hypyn suunta
+	private Vector3 lastMotion;
+	private CharacterController controller;
+
+	/*Animaatio STATET---------------------------------------------
+	 * State 0 = Ase stance(idle)
+	 * State 1 = Run with hands
+	 * State 2 = Jump Up
+	 * State 3 = Falling
+	 * State 4 = attackk
+	 * State 5 = walljump
+	 *--------------------------------------------------------------
+	*/
+	// Use this for initialization
+	void Start()
+	{
+		//_animeScript = GetComponent<AnimeController> ();
+		controller = GetComponent<CharacterController>();
+		anime = GetComponent<Animator>();
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//LIIKKUMINEN JA FLIP, Run ja Idle animaatio ----------------------------------------------------------------
+		IsControllerGrounded();
+		moveVector = Vector3.zero;
+		inputDirection = Input.GetAxis("P2movement") * speed;
+		value = Input.GetAxis("P2movement");
+		//Debug.Log(value);
+		if (value > 0)
+		{
+
+			if (_facingRight == false)
+			{
+				Flip();
+			}
+			if (IsControllerGrounded()) 
+			{	
+				anime.SetInteger ("State", 1);
+			}
+		}
+
+		if (value < 0)
+		{
+
+			if (_facingRight == true)
+			{
+				Flip();
+			}
+			if (IsControllerGrounded()) 
+			{	
+				anime.SetInteger ("State", 1);
+			}
+		}
+		if (value == 0 && verticalVelocity == 0)
+		{
+			anime.SetInteger ("State", 0);
+		}
+
+		//----------------------------------------------------------------------------
+
+		//HYÖKKÄYS ANIMAATIO (hyökkäys komento itsessään on playeruseskill.cs)
+
+		if (Input.GetButtonDown("P2Fire"))
+		{
+			anime.SetInteger ("State", 4);
+		}
+
+		//----------------------------------------------------------------------------
+		//HYPPY, TUPLAHYPPY ja molempien animaatiot
+		if (IsControllerGrounded())
+		{
+			verticalVelocity = 0;
+
+			if (Input.GetButtonDown("P2Jump"))
+			{
+				anime.SetInteger ("State", 2);
+				verticalVelocity = jumpForce;
+				//Kun ilmassa secondjump on aktiivinen
+				secondJumpAvail = true;
+			}
+			moveVector.x = inputDirection;
+		}
+		else
+		{
+
+			if (Input.GetButtonDown("P2Jump"))
+			{
+				if(secondJumpAvail)
+				{
+					anime.SetInteger ("State", 2);
+					verticalVelocity = jumpForce;
+					secondJumpAvail = false;
+				}
+			}
+
+			verticalVelocity -= gravity * Time.deltaTime;
+			//Jos haluat vapaan liikkumisen ja vapaan hyppy suunnan, ota kaksi seuraavaa käyttöön
+			//moveVector.x = inputDirection;
+			//moveVector.y = inputDirection;
+			//Jos haluat fixedjump ota käyttöön
+			moveVector.x = lastMotion.x;
+		}
+
+		moveVector.y = verticalVelocity;
+		//  moveVector = new Vector3(inputDirection, verticalVelocity, 0);
+		controller.Move(moveVector * Time.deltaTime);
+		lastMotion = moveVector;
+
+	}
+
+	//-----------------------------------------------------------------------------------------------
+
+	void FixedUpdate()
+	{   
+		//tässä FALLING animaation laukaisu rigidbodylla
+
+		//Debug.Log(moveVector.y);
+		if (moveVector.y != 0f /*&& Input.GetButtonUp("P1Jump")*/)
+		{
+			Debug.Log("hyppasin, menen up ja hyppy ei pohjassa");
+			anime.SetInteger ("State", 3);
+
+		}
+		/*if (moveVector.y < 0f)
+        {
+            Debug.Log("tipun");
+			anime.SetInteger ("State", 3);
+			//_animeScript.FallingAnimation ();
+        }*/
+	}
+	//-------------------------------------------------------------------------------------
+
+	// GROUNDED CHECK RAYCASTILLA
+	// Raycast grounded check https://www.youtube.com/watch?v=8Cado6CzZUA&list=PLLH3mUGkfFCWwekOW1OMxyyIgc-Qm1OhI&index=10
+	private bool IsControllerGrounded()
+	{
+		Vector3 leftRayStart;
+		Vector3 rightRayStart;
+
+		leftRayStart = controller.bounds.center;
+		rightRayStart = controller.bounds.center;
+
+		leftRayStart.x -= controller.bounds.extents.x;
+		rightRayStart.x += controller.bounds.extents.x;
+
+		Debug.DrawRay(leftRayStart, Vector3.down, Color.red);
+		Debug.DrawRay(rightRayStart, Vector3.down, Color.blue);
+
+		if (Physics.Raycast(rightRayStart, Vector3.down, (controller.height / 2) + hoverHeight))
+		{
+			//Debug.Log("osuu varpaat");
+			return true;
+		}
+
+
+		if (Physics.Raycast(leftRayStart, Vector3.down, (controller.height / 2) + hoverHeight))
+		{
+			//Debug.Log("osuu kantapaa");
+			return true;
+		}
+
+		//Debug.Log("ei osu maahan");
+		return false;
+
+	}
+	//------------------------------------------------------------------------------------
+
+	//TÄSSÄ WALLJUMP JA SEN ANIMAATIO
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if(controller.collisionFlags == CollisionFlags.Sides)
+		{
+			if(Input.GetButtonDown("P2Jump"))
+			{
+				anime.SetInteger ("State", 5);
+				moveVector = hit.normal * speed;
+				verticalVelocity = jumpForce;
+			}
+
+		}
+	}
+	//--------------------------------------------------------------------------------------
+
+	//TÄSSÄ MÄÄRITELTIIN FLIPPI
+	private void Flip()
+	{
+		_facingRight = !_facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.z *= -1;
+		transform.localScale = theScale;
+	}
+
+}
