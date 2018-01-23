033155619446424d2ef3339f26476bd84b16983e
DESKTOP-AA7SO01\lauri <lauri.syvanen@hotmail.com>
2018-01-23 12:10:41 +0200

    fallkillboxit poistettu

---
+++ Project5_3ViikonSuperProjekti/Assets/Script/tmp_scriptit/Laurin tekeillä olevat/ButtonControlsHelp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControlsHelp : MonoBehaviour 
+{
+	public GameObject Ohje;
+
+	void update()
+	{
+		
+	}
+}
