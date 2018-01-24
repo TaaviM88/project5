9ffb69290abb4fb16d0777721a36a526d10ab8bd
DESKTOP-AA7SO01\lauri <lauri.syvanen@hotmail.com>
2018-01-24 11:10:18 +0200

    ohje ruutu toimintaan

--- Project5_3ViikonSuperProjekti/Assets/Script/tmp_scriptit/Laurin tekeillä olevat/ButtonControlsHelp.cs
+++ Project5_3ViikonSuperProjekti/Assets/Script/tmp_scriptit/Laurin tekeillä olevat/ButtonControlsHelp.cs
@@ -1,13 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonControlsHelp : MonoBehaviour 
 {
 	public GameObject Ohje;
+	public Button yourButton;
+	bool isOhjeOn;
 
-	void update()
+	void Awake()
 	{
-		
+		isOhjeOn = false;
+		Debug.Log ("ohje ei ole päällä");
+		//Ohje.SetActive (false);
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+
 	}
-}
+
+	void TaskOnClick()
+	{
+		if (isOhjeOn == false)
+		{
+			Debug.Log ("Ohje päällä");
+			Ohje.SetActive(true);
+			isOhjeOn = true;
+		}
+		else if (isOhjeOn == true)
+		{
+			Debug.Log ("ohje latettiin pois päältä");
+			Ohje.SetActive(false);
+			isOhjeOn = false;
+		}
+	}
+
+
+	}
